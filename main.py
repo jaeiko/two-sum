a17d7e0fc3cf796e6bd7c7d2e816eaae0ed36ca7
jaeiko <jkimkr08@daum.net>
2025-04-16 19:04:01 +0900

    Remove input, output

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
-
-
-num_list = list(map(int, input("nums = ").split()))
-target = int(input("Enter target : "))
-
-answer_list = twoSum(num_list, target)
-print(answer_list)
